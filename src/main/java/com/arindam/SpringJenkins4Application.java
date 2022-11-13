370c4c53863b99a3970a13ba63a8ff57568d4cfc
arindamslt <arin.saltlake@gmail.com>
2022-11-13 22:03:20 +0530

    first commit

---
+++ src/main/java/com/arindam/SpringJenkins4Application.java
@@ -0,0 +1,13 @@
+package com.arindam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJenkins4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJenkins4Application.class, args);
+	}
+
+}
